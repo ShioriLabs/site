4d73c7fee50e5af6568f87bff953b6a837412e7e
Dhika Rizky <deka_r@outlook.com>
2021-01-28 02:58:17 +0700

    Added default font size

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,7 @@ const GlobalStyle = createGlobalStyle`
     color: ${props => props.theme.colors.black};
     font-weight: ${props => props.theme.fontWeights.normal};
     font-family: "Quicksand", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+    font-size: 18px;
   }
 `
 

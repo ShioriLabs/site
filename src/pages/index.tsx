010ba3a577a14efe529ee4d03d3bf53c188c822f
Dhika Rizky <deka_r@outlook.com>
2021-01-28 01:15:40 +0700

    Added some filler section in homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import Layout from '../layout/Default'
 
 import Container from '../components/Container'
 import Hero, { HeroHeader, HeroSubheader, HeroTextContainer } from '../components/Hero'
+import HomeSection, { HomeSectionTextContainer, HomeSectionTitle, HomeSectionText } from '../components/HomeSection'
 
 const Index = (): React.ReactElement => {
   return (
@@ -20,6 +21,30 @@ const Index = (): React.ReactElement => {
           </HeroTextContainer>
         </Hero>
       </Container>
+      <HomeSection invertedBackground>
+        <HomeSectionTextContainer>
+          <HomeSectionTitle>Kami adalah Shiori Labs</HomeSectionTitle>
+          <HomeSectionText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pengen megane gf gan.
+          </HomeSectionText>
+        </HomeSectionTextContainer>
+      </HomeSection>
+      <HomeSection invertedDirection>
+        <HomeSectionTextContainer>
+          <HomeSectionTitle>Kami adalah Shiori Labs</HomeSectionTitle>
+          <HomeSectionText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pengen megane gf gan.
+          </HomeSectionText>
+        </HomeSectionTextContainer>
+      </HomeSection>
+      <HomeSection invertedBackground>
+        <HomeSectionTextContainer>
+          <HomeSectionTitle>Kami adalah Shiori Labs</HomeSectionTitle>
+          <HomeSectionText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pengen megane gf gan.
+          </HomeSectionText>
+        </HomeSectionTextContainer>
+      </HomeSection>
     </Layout>
   )
 }

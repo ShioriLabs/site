31bc276951a07cebec2aec2f89420654717d2336
Dhika Rizky <deka_r@outlook.com>
2021-01-28 00:03:15 +0700

    Added basic hero component

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+const Hero = styled.div`
+  min-height: 560px;
+  display: flex;
+  align-items: center;
+`
+
+const HeroTextContainer = styled.div`
+  max-width: 560px;
+`
+
+const HeroHeader = styled.h1`
+  font-size: 48px;
+  font-weight: ${props => props.theme.fontWeights.bold};
+  color: ${props => props.theme.colors.branding};
+  margin: 0px;
+  margin-bottom: 32px;
+`
+
+const HeroSubheader = styled.p`
+  font-size: 24px;
+  margin: 0px;
+`
+
+export default Hero
+export { HeroHeader, HeroSubheader, HeroTextContainer }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,20 @@ import React from 'react'
 
 import Layout from '../layout/Default'
 
+import Container from '../components/Container'
+import Hero, { HeroHeader, HeroSubheader, HeroTextContainer } from '../components/Hero'
+
 const Index = (): React.ReactElement => {
   return (
     <Layout>
-      <p>Hello World</p>
+      <Container>
+        <Hero>
+          <HeroTextContainer>
+            <HeroHeader>Sebuah Tempat Dimana Kesenangan Dibuat</HeroHeader>
+            <HeroSubheader>Kami Shiori Labs, dan kami membuat hal-hal yang menyenangkan!</HeroSubheader>
+          </HeroTextContainer>
+        </Hero>
+      </Container>
     </Layout>
   )
 }

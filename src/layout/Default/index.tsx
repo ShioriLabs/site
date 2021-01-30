7d4bd310fb8d8e7d9fbab9ce56c139adb6bf92f2
Dhika Rizky <deka_r@outlook.com>
2021-01-31 03:45:31 +0700

    Added basic footer

--- src/layout/Default/index.tsx
+++ src/layout/Default/index.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import Navbar from '../../components/Navbar'
+import Footer from '../../components/Footer'
+
+const Content = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
 
 interface Props {
   children: React.ReactElement | React.ReactElement[] | string | number
@@ -8,10 +16,11 @@ interface Props {
 
 const DefaultLayout = ({ children }: Props): React.ReactElement => {
   return (
-    <>
+    <Content>
       <Navbar />
       {children}
-    </>
+      <Footer />
+    </Content>
   )
 }
 
